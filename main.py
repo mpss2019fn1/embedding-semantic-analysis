f6d8f4a83073f0b7e2c9e328bb88fafca201558c
Hung Nguyen <hungn@outlook.de>
2019-06-11 17:53:36 +0200

    add networkx graph using wikipedia as knowledgebase for semantic analysis
    Co-authored-by: Alexander Riese <alexander.riese@student.hpi.de>

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 from wikidata_endpoint import WikidataEndpoint, WikidataEndpointConfiguration
 from pathlib import Path
+
+member = dict()
+DG = nx.DiGraph()
+root = 'ROOT'
+DG.add_node(root)
+
+
+def graph_insertion(current_set_name, root_name):
+    if member[root_name].issuperset(member[current_set_name]):
+        already_inserted = False
+        for child in DG.neighbors(root_name):
+            already_inserted = already_inserted or graph_insertion(current_set_name, child)
+        if not already_inserted:
+            DG.add_edge(root_name, current_set_name)
+        return True
+    else:
+        return False
 
 
 def main():
@@ -8,14 +28,21 @@ def main():
     parser.add_argument('wikidata_ids', metavar='N', type=int, nargs='+')
     args = parser.parse_args()
     wikidata_ids = args.wikidata_ids
-    wikidata_ids = range(1000)
     endpoint = WikidataEndpoint(WikidataEndpointConfiguration(Path("resources/wikidata_endpoint_config.ini")))
 
     query = open('resources/query.rq').read() % '\n'.join(f'wd:Q{wikidata_id}' for wikidata_id in wikidata_ids)
+    member['ROOT'] = set(wikidata_ids)
 
     with endpoint.request() as request:
         for answer in request.post(query):
-            print(answer)
+            current_set = (answer['member'].split(', '))
+            current_set_name = answer['object'].split('/Q')[-1]
+            member[current_set_name] = current_set
+            DG.add_node(current_set_name)
+            graph_insertion(current_set_name, root)
+    print(list(DG.nodes(data=True)))
+    nx.draw(DG)
+    plt.show()
 
 
 if __name__ == '__main__':
