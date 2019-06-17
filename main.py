f9d51f7611ef9aba8a7a8093e4e9e18c98718910
Alexander Riese <alexander.riese@student.hpi.de>
2019-06-17 14:10:46 +0200

    Use asynchronous processing

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from collections import defaultdict
+from aiostream import stream
+import asyncio
 
 from wikidata_endpoint import WikidataEndpoint, WikidataEndpointConfiguration
 
 
-def get_relations(wikidata_id: int, endpoint: WikidataEndpoint):
+async def get_relations(wikidata_id: int, endpoint: WikidataEndpoint):
     query = open('resources/get_relations.rq').read() % f'wd:Q{wikidata_id}'
 
     with endpoint.request() as request:
         for results in request.post(query):
             subject, predicate, object_ = [result.split('/entity/')[-1] for result in results.values()]
-            breakpoint()
+            yield (subject, predicate, object_)
 
-# person ?wd ?ps_
 
-def main():
+async def main():
     parser = ArgumentParser()
     parser.add_argument('wikidata_ids', metavar='N', type=int, nargs='+')
     args = parser.parse_args()
@@ -24,14 +25,12 @@ def main():
 
     relations_entity_map = defaultdict(set)
 
-    for wikidata_id in wikidata_ids:
-        relations = get_relations(wikidata_id, endpoint)
-        for relation in relations:
-            relations_entity_map[relation].add(wikidata_id)
-
-
-
+    async with stream.chain(
+            *[get_relations(wikidata_id, endpoint) for wikidata_id in wikidata_ids]).stream() as relations:
+        async for relation in relations:
+            subject, predicate, object_ = relation
+            relations_entity_map[(predicate, object_)].add(subject)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
