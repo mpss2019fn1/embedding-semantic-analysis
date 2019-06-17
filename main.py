7726cc4a63f3fca09842f7b80a3250044527c1f9
Alexander Riese <alexander.riese@student.hpi.de>
2019-06-17 09:58:00 +0200

    fix graph creation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import networkx as nx
-import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 from wikidata_endpoint import WikidataEndpoint, WikidataEndpointConfiguration
@@ -14,7 +13,7 @@ DG.add_node(root)
 def graph_insertion(current_set_name, root_name):
     if member[root_name].issuperset(member[current_set_name]):
         already_inserted = False
-        for child in DG.neighbors(root_name):
+        for child in DG.successors(root_name):
             already_inserted = already_inserted or graph_insertion(current_set_name, child)
         if not already_inserted:
             DG.add_edge(root_name, current_set_name)
@@ -31,17 +30,17 @@ def main():
     endpoint = WikidataEndpoint(WikidataEndpointConfiguration(Path("resources/wikidata_endpoint_config.ini")))
 
     query = open('resources/query.rq').read() % '\n'.join(f'wd:Q{wikidata_id}' for wikidata_id in wikidata_ids)
-    member['ROOT'] = set(wikidata_ids)
+    member['ROOT'] = set(f'http://www.wikidata.org/entity/Q{wikidata_id}'for wikidata_id in wikidata_ids)
 
     with endpoint.request() as request:
         for answer in request.post(query):
-            current_set = (answer['member'].split(', '))
+            current_set = set(answer['member'].split(', '))
             current_set_name = answer['object'].split('/Q')[-1]
             member[current_set_name] = current_set
             DG.add_node(current_set_name)
             graph_insertion(current_set_name, root)
     print(list(DG.nodes(data=True)))
-    nx.draw(DG)
+    nx.draw_networkx(DG, with_labels=True)
     plt.show()
 
 
