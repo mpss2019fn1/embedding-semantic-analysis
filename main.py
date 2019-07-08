2c16008c0af32ccc663a1d61b2f5c324b1a9db52
Alexander Riese <alexander.riese@student.hpi.de>
2019-07-08 12:26:09 +0200

    Improve relation selection
    Co-authored-by: Hung Nguyen <hungn@outlook.de>

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import asyncio
 from argparse import ArgumentParser
+from pathlib import Path
+import pickle
+from itertools import combinations
 
 from relation_fetcher import RelationFetcher
-from relation_selector import RelationSelector
+from relation_selector import RelationSelector, overlap_coefficient
 
 
 async def main():
     parser = ArgumentParser()
     parser.add_argument('wikidata_ids', metavar='N', type=int, nargs='+')
     args = parser.parse_args()
-    wikidata_ids = range(20)
-
-    relation_fetcher = RelationFetcher(wikidata_ids)
-    relation_mapping = await relation_fetcher.fetch()
+    wikidata_ids = range(1000)
+    if Path('relation_mapping.pickle').exists():
+        with open('relation_mapping.pickle', 'rb') as handle:
+            relation_mapping = pickle.load(handle)
+    else:
+        relation_fetcher = RelationFetcher(wikidata_ids)
+        relation_mapping = await relation_fetcher.fetch()
     relation_selector = RelationSelector(relation_mapping)
-    print(relation_selector.global_relation_counter())
+    print(len(relation_selector.relations_mapping))
+    relation_selector.remove_unique_relations()
+    print(len(relation_selector.relations_mapping))
+    relation_selector.remove_rare_relations(0.3)
+    print(len(relation_selector.relations_mapping))
+    relation_selector.remove_unique_relations()
+    print(len(relation_selector.relations_mapping))
+    breakpoint()
+
+    # selected_predicates = [x[0] for x in relation_selector.group_counter().items() if 3 < x[1]]
+    # selected_predicate_groups = {
+    #     selected_predicate: {x[1] for x in relation_selector.relations_mapping if x[0] == selected_predicate} for
+    #     selected_predicate in selected_predicates}
+    # for predicate, objects in selected_predicate_groups.items():
+    #     print('==========================')
+    #     print(predicate)
+    #     groups = (relation_selector.relations_mapping[(predicate, object_)] for object_ in objects)
+    #     for (group1, label1), (group2, label2) in combinations(zip(groups, objects), 2):
+    #         if len(group1) == 1 or len(group2) == 1:
+    #             continue
+    #         overlap_coefficient_value = overlap_coefficient(group1, group2)
+    #         if overlap_coefficient_value == 0.0:
+    #             print(label1.sparql_escape() + ' ' + label2.sparql_escape())
+    #             print(f'{predicate} {len(group1)} {len(group2)} {overlap_coefficient_value}')
 
 
 if __name__ == '__main__':
--- relation_selector.py
+++ relation_selector.py
@@ -11,6 +11,10 @@ class TimeoutException(Exception):
     pass
 
 
+def overlap_coefficient(set1, set2):
+    return len(set1 & set2) / min(len(set1), len(set2))
+
+
 def chunk_dictionary(dictionary, chunk_size):
     it = iter(dictionary)
     for _ in range(0, len(dictionary), chunk_size):
@@ -21,18 +25,19 @@ class RelationSelector:
 
     def __init__(self, relations_mapping, endpoint=None):
         self.relations_mapping = relations_mapping
+        self.number_entities = len(set().union(*relations_mapping.values()))
         self.endpoint = endpoint or WikidataEndpoint(
             WikidataEndpointConfiguration(Path("resources/wikidata_endpoint_config.ini")))
 
-    @lru_cache(maxsize=None)
+    # @lru_cache(maxsize=None)
     def relation_counter(self, threshold=1):
         return Counter({key: len(value) for key, value in self.relations_mapping.items() if len(value) > threshold})
 
-    @lru_cache(maxsize=None)
+    # @lru_cache(maxsize=None)
     def group_counter(self):
         return Counter(c[0] for c in self.relation_counter())
 
-    @lru_cache(maxsize=None)
+    # @lru_cache(maxsize=None)
     def global_relation_counter(self, chunk_size=800):
         global_count = Counter()
         chunks = chunk_dictionary(self.relations_mapping, chunk_size)
@@ -43,7 +48,8 @@ class RelationSelector:
                     f'({x[0].sparql_escape()} {x[1].sparql_escape()})' for x in chunk)
                 with self.endpoint.request() as request:
                     try:
-                        request_result = request.post(global_query, on_timeout=lambda x: exec('raise(TimeoutException)'))
+                        request_result = request.post(global_query,
+                                                      on_timeout=lambda x: exec('raise(TimeoutException)'))
                     except TimeoutException:
                         logging.error("Request timeout: Will try again with smaller chunks")
                         timeout_chunks.append(chunk_dictionary(chunk, 3))
@@ -53,6 +59,17 @@ class RelationSelector:
                 global_count.update(chunk_global_counts)
             chunks = timeout_chunks
         return global_count
+
+    def remove_unique_relations(self):
+        self.relations_mapping = {key: value for key, value in self.relations_mapping.items() if len(value) > 1}
+
+    def remove_rare_relations(self, threshold):
+        group_counter = self.group_counter()
+        self.relations_mapping = {key: value for key, value in self.relations_mapping.items() if
+                                  group_counter[key[0]] > threshold * self.number_entities}
+
+    def remove_single_groups(self):
+        self.relations_mapping = {key: value for key, value in self.relations_mapping.items()}
 
     def score(self, relation):
         pass
