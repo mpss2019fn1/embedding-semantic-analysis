0354877b497f3f1c868468e65d3d04e6f40ca413
Hung Nguyen <hungn@outlook.de>
2019-07-24 14:36:46 +0200

    implement evaluation_set_config_generator
    Co-authored-by: Mathias Flueggen <mflueggen@uos.de>

--- evaluation_set_config_generator.py
+++ evaluation_set_config_generator.py
@@ -6,16 +6,20 @@ import os
 
 @dataclass
 class Task:
-    name : str
-    type : str
-    test_set : str
+    name: str
+    type: str
+    test_set: str
+
 
 @dataclass
 class Category:
-    name : str
-    enabled : bool
-    categories : dict
-    tasks : list
+    name: str
+    enabled: bool
+    categories: dict
+    tasks: list
+
+    def to_yaml(self):
+        return 'ok'
 
 
 class EvaluationSetConfigGenerator:
@@ -25,31 +29,46 @@ class EvaluationSetConfigGenerator:
 
     @staticmethod
     def build_from_file_system(root_dir):
-        dict_hierarchy = dict()
+        root_category = Category(name="root", enabled=True, categories={}, tasks=[])
+        # dict_hierarchy = dict()
         for root, dirs, files in os.walk(root_dir, topdown=False):
             for file in files:
                 if file.endswith('.csv'):
                     path = root + '/' + file
+                    #print(path)
                     split_path = path.split('/')
-                    previous_hierarchy_level = dict_hierarchy
-                    for i in range(1,len(split_path)-2):
-                        key = f'{split_path[i]}({split_path[i+1]})'
-                        current_hierarchy_level = previous_hierarchy_level.get(key, None)
-                        if current_hierarchy_level is None:
-                            current_hierarchy_level = dict()
-                        previous_hierarchy_level = current_hierarchy_level
+                    previous_category = root_category
+                    for i in range(1, len(split_path) - 2):
+                        category_key = f'{split_path[i]}({split_path[i + 1]})'
+                        current_category = previous_category.categories.get(category_key, None)
+                        if current_category is None:
+                            current_category = Category(name=category_key, enabled=True, categories={}, tasks=[])
+                            previous_category.categories[category_key] = current_category
+                        previous_category = current_category
 
                     filename = split_path[-1].split('.csv')[0]
-                    value = f'{split_path[-2]}({filename})'
-                    #previous_hierarchy_level[]
+                    key = f'{split_path[-2]}({filename})'
+                    previous_category.categories[key] = Category(name=key, enabled=True, categories={}, tasks=[])
+        #print(yaml.dump(root_category))
 
+        print(yaml.dump({"tasks": [{"task": {"name": "hung", "name2": "mathias"}}, {"task": {"name": "chick1", "name2": "chick2"}}]}))
+        #
+        # with open('test.yaml', "r") as stream:
+        #     configuration = yaml.safe_load(stream)
+        # breakpoint()breakpoint
 
-
-
-
-    def save_config(self):
-        pass
 
 
 if __name__ == '__main__':
     EvaluationSetConfigGenerator.build_from_file_system('root_2')
+
+    # yaml.dump({'name': 'human',
+    #            'enabled': True,
+    #            'tasks': {name: 'male <-> female (Analogy Task)', type: 'analogy' , test_set: path}
+    #            'categories': { categiry ; {'name': 'stuff',
+    #                            'enabled': True,
+    #                            'tasks': {},
+    #                            'categories': }},
+    #
+    #
+    # }})
