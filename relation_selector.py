45fe10cb2059aa99bb3400bfc369a96c747e8aa3
Alexander Riese <alexander.riese@student.hpi.de>
2019-08-10 15:16:58 +0200

    refactor relation selection and improve performance

--- relation_selector.py
+++ relation_selector.py
@@ -24,8 +24,9 @@ class RelationSelector:
         self.property_mapping = relations_mapping
         self.endpoint = endpoint or WikidataEndpoint(
             WikidataEndpointConfiguration(Path("resources/wikidata_endpoint_config.ini")))
-        self.metric_data_frame = pd.DataFrame()
+        self.metric_data_frame = pd.DataFrame([*self.relation_groups().keys()], columns=['predicate'])
         self.metric_config_path = metric_config_path
+        self.number_entities = len(set().union(*self.property_mapping.values()))
 
     def top_property(self, not_include):
         metric_weights = []
@@ -33,14 +34,9 @@ class RelationSelector:
             metric_reader = csv.reader(metric_csv_file)
             # skip header
             next(metric_reader)
-            metric_name, metric_weight = next(metric_reader)
-            self.metric_data_frame = pd.DataFrame(list(metrics[metric_name](self).items()),
-                                                  columns=['predicate', metric_name])
-            self.metric_data_frame[metric_name] = self.metric_data_frame[metric_name].astype('float64')
-            metric_weights.append(float(metric_weight))
             for metric_name, metric_weight in metric_reader:
                 self.metric_data_frame[metric_name] = self.metric_data_frame['predicate'].map(
-                    metrics[metric_name](self)).astype('float64')
+                    lambda predicate: metrics[metric_name](self, predicate)).astype('float64')
                 metric_weights.append(float(metric_weight))
         self.metric_data_frame['score'] = self.metric_data_frame.drop(['predicate'], axis=1).to_numpy() @ np.array(
             metric_weights)
@@ -50,33 +46,26 @@ class RelationSelector:
         return self.metric_data_frame.loc[self.metric_data_frame['score'].idxmax()]['predicate']
 
     @metric
-    def popularity(self):
-        """
-        :return: mapping from predicate to proportion of entities, which have a outgoing edge with this predicate
-        """
-        result = dict()
-        number_entities = len(set().union(*self.property_mapping.values()))
-        for predicate, objects in self.relation_groups().items():
-            predicate_objects = set()
-            for object_ in objects:
-                predicate_objects = predicate_objects.union(self.property_mapping[(predicate, object_)])
-            result[predicate] = len(predicate_objects) / number_entities
-        return result
+    def popularity(self, predicate) -> float:
+        objects = self.relation_groups()[predicate]
+        predicate_objects = set()
+        for object_ in objects:
+            predicate_objects = predicate_objects.union(self.property_mapping[(predicate, object_)])
+        return len(predicate_objects) / self.number_entities
 
     @metric
-    def big_groups(self, number_of_big_groups=3):
-        result = dict()
-        for predicate, objects in self.relation_groups().items():
-            heap = []
-            total_sum = 0
-            for object_ in objects:
-                group_size = len(self.property_mapping[(predicate, object_)])
-                total_sum += group_size
-                heappushpop(heap, group_size) if len(heap) > number_of_big_groups else heappush(heap, group_size)
-            big_groups_sum = sum(nlargest(number_of_big_groups, heap))
-            result[predicate] = big_groups_sum / total_sum
-        return result
+    def big_groups(self, predicate, number_of_big_groups=3) -> float:
+        objects = self.relation_groups()[predicate]
+        heap = []
+        total_sum = 0
+        for object_ in objects:
+            group_size = len(self.property_mapping[(predicate, object_)])
+            total_sum += group_size
+            heappushpop(heap, group_size) if len(heap) > number_of_big_groups else heappush(heap, group_size)
+        big_groups_sum = sum(nlargest(number_of_big_groups, heap))
+        return big_groups_sum / total_sum
 
+    @lru_cache(maxsize=None)
     def relation_groups(self):
         """
         :return: mapping from predicate to connected objects
