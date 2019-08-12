45b039d6cccd612b4fdac503168f1451a34ed836
mapp <mflueggen@student.hpi.de>
2019-08-12 14:29:25 -0400

    Fixes outlier Task. Hung is hung!
    Co-authored-by: Hung Nguyen <hungn@outlook.de>

--- task_creator.py
+++ task_creator.py
@@ -199,7 +199,7 @@ class OutlierTaskCreator(TaskCreator):
         for entity in entities:
             entities_group.append([entity, cluster_id, False])  # entity, group_id, is_outlier
             if len(entities_group) == self.max_group_size - 1:
-                outlier = self.get_outlier(path)
+                outlier = self.get_outlier(path, node)
                 if outlier:
                     entities_group.append([outlier, cluster_id, True])  # entity, group_id, is_outlier
                     content.extend(entities_group)
@@ -209,54 +209,37 @@ class OutlierTaskCreator(TaskCreator):
         if len(content) > 3:
             TaskCreator.save_to_file(self.filename_from_path(path), content)
 
-    def get_outlier(self, path):
-        split_path = path.split('/')
-        level = 1  # level of first predicate by which was split
-        stack = [self.root_node]
-        outlier_exists = False
-        while stack:
-            current_node = stack.pop()
-            if current_node.is_leaf():
-                if outlier_exists:
-                    # select random outlier
-                    values_as_list = list(current_node.values)
-                    i = random.randint(0, len(values_as_list) - 1)
-                    wikidata_id = HierarchyTraversal.extract_wikidata_id(values_as_list[i].value)
-                    return wikidata_id
-                else:
-                    return None
+    def get_outlier(self, path, neighborhood_node):
+        path_set = set(path.split('/')[2::2])
 
+        stack = [self.root_node]
+
+        while stack:
+            current_node = stack[-1]
+            if current_node.is_leaf():
+                # select random outlier
+                values = list(current_node.values - neighborhood_node.values)
+                if values:
+                    return HierarchyTraversal.extract_wikidata_id(values[random.randint(0, len(values) - 1)].value)
+                else:
+                    stack.pop()
+                    continue
             # try to select a child along a different path
             # select random child with object != split_path[level + 1]
-            object_to_exclude = ""
-            if level < len(split_path):
-                object_to_exclude = split_path[level + 1]  # object in path
-            child, on_different_path = OutlierTaskCreator._select_random_child(current_node, object_to_exclude)
-            outlier_exists = outlier_exists or on_different_path
-            stack.append(child)
+            random_children = OutlierTaskCreator._select_random_children(current_node, path_set)
+            if random_children:
+                stack.extend(random_children)
 
-            level += 2
-
-        raise Exception("This line should never be reached")
+        return None
 
     @staticmethod
-    def _select_random_child(node, object_to_exclude):
-        child_count = len(node.children)
-        assert child_count > 0, "node must have at least one child"
-
-        rdf_objects = [HierarchyTraversal.extract_wikidata_id(child.label[1].value) for child in node.children]
-
-        selected_index = -1
-
-        if child_count == 1:
-            selected_index = 0
-        else:
-            while selected_index < 0:
-                i = random.randint(0, child_count - 1)
-                if rdf_objects[i] != object_to_exclude:
-                    selected_index = i
-
-        return node.children[selected_index], rdf_objects[selected_index] != object_to_exclude
+    def _select_random_children(node, objects_to_exclude):
+        rdf_objects = [child for child in node.children if
+                       HierarchyTraversal.extract_wikidata_id(child.label[1].value) not in objects_to_exclude]
+        if not rdf_objects:
+            return None
+        random.shuffle(rdf_objects)
+        return rdf_objects
 
 
 class EntityCollectorTaskCreator(TaskCreator):
