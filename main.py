00a05fc5a7e02257a24935754bb694073284c775
mapp <mflueggen@student.hpi.de>
2019-08-15 08:03:24 -0400

    Increases group size for outliers

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def main():
     hierachy_builder.save_to_file('hierarchy_leaf_data.csv')
 
     neighborhood_task_creator = NeighborhoodTaskCreator(args.output_dir)
-    outlier_task_creator = OutlierTaskCreator(args.output_dir, hierachy_builder, 3)
+    outlier_task_creator = OutlierTaskCreator(args.output_dir, hierachy_builder, 5)
     analogy_task_creator = AnalogyTaskCreator(args.output_dir, wikidata_ids)
     similarity_task_creator = SimilarityTaskCreator(args.output_dir, hierachy_builder)
     get_entities_task_creator = EntityCollectorTaskCreator(args.output_dir)
